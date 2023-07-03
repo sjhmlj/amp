a064e07f1c309f25d03442230ea9d17bcf651b5c
sjhmlj <sjhmlj@gmail.com>
2023-07-03 17:43:02 +0900

    23.7.3

--- src/data/dataset_test.py
+++ src/data/dataset_test.py
@@ -1,7 +1,14 @@
-from make_dataset import DataPrep, CustomDataset
+from make_dataset import ShowData, CustomDataset
+from IPython.display import display
 
 path = '/home/jh/PycharmProjects/amp/data'
 
-data = DataPrep(path, 'train_peptides.csv', 'train_proteins.csv', 'train_clinical_data.csv', 'supplemental_clinical_data.csv')
+data = ShowData(path, 'train_peptides.csv', 'train_proteins.csv', 'train_clinical_data.csv', 'supplemental_clinical_data.csv')
 
-print(data.show_df_shape(), data.show_null_count(), sep='\n')
+# print(data.show_df_shape(), data.show_null_count(), sep='\n')
+# display(data.display_df_head())
+# data.plot_null_count(x='visit_month', y='upd23b_clinical_state_on_medication')
+
+# print(data.show_null_count())
+# data.plot_updrs_on_md()
+data.plot_null_count_abt_updrs()
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,11 +1,16 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from plotly.subplots import make_subplots
 from torch.utils.data import Dataset, DataLoader
-
+import plotly.graph_objects as go
+from IPython.display import display
 print(repr(Dataset))
 
 # for EDA, merging data
-class DataPrep(object):
+class ShowData(object):
     def __init__(self, path, pep_df, pro_df, cli_df, suppl_df=None):
         self.pep = pd.read_csv(f'{path}/{pep_df}')
         self.pro = pd.read_csv(f'{path}/{pro_df}')
@@ -14,7 +19,7 @@ class DataPrep(object):
         self.df_name_list = ['peptides', 'proteins', 'clinical', 'supplemental']
         self.data = [self.pep, self.pro, self.cli, self.suppl]
 
-    def show_df_shape(self):
+    def show_shape(self):
         df_shape_list = []
         for i in self.data:
             if i is not None:
@@ -28,10 +33,65 @@ class DataPrep(object):
                 null_count_list.append((i.isna().sum()))
         return dict(zip(self.df_name_list, null_count_list))
 
-    def
+    def display_head(self):
+        df_head_list = []
+        for i in self.data:
+            df_head_list.append(i.head())
+        return df_head_list
+
+    def plot_null_count(self, df_num=2, x='visit_month', y='upd23b_clinical_state_on_medication', width=800, height=500, title_font_size=20):
+        ''' df_num to distinguish which df is needed'''
+        df = self.data[df_num].copy()
+        df[y] = df[y].fillna('Null')
+
+        fig = px.histogram(df, x=x, color=y, title=f'count of {x}', color_discrete_sequence=px.colors.qualitative.Vivid,
+                           width=width, height=height)
+        fig.update_layout(template='plotly_dark')
+        fig.update_layout(title_font_size=title_font_size)
+        fig.show()
+
+    def plot_null_count_abt_updrs(self, df_num=2, y='upd23b_clinical_state_on_medication', width=800, height=500, title_font_size=20):
+        df = self.data[df_num].copy()
+        df[y] = df[y].fillna('Null')
+
+        fig = make_subplots(rows=4, cols=1)
+        for i, updrs_part in enumerate(['1', '2', '3', '4']):
+            # fig_ = px.histogram(df, x=f'updrs_{updrs_part}', color=y, title=f'count of updrs part {updrs_part} score ({self.df_name_list[df_num]})', color_discrete_sequence=px.colors.qualitative.Vivid,
+            #                width=width, height=height)
+            for j in ['On', 'Off', 'Null']:
+                df_ = df.query('upd23b_clinical_state_on_medication in @j')
+                fig_ = go.Histogram(x=df[f'updrs_{updrs_part}'], y=df_[y])
+                fig.add_trace(fig_, row=i+1, col=1)
+            fig.update_yaxes(title_text=f'UPDRS Part {updrs_part}', row=i+1)
+        fig.update_layout(template='plotly_dark')
+        fig.update_layout(title_font_size=title_font_size)
+        fig.show()
+
+    def plot_updrs_on_md(self, ):
+        df = self.data[2].copy()
+        df['upd23b_clinical_state_on_medication'] = df['upd23b_clinical_state_on_medication'].fillna('Null')
+
+        parts = ['1', '2', '3', '4']
+        fig = make_subplots(rows=4, cols=2,
+                            horizontal_spacing=0.10,
+                            vertical_spacing=0.06,
+                            column_titles=['Medication: "On"', 'Medication: "Off" or "Null"'])
+        for i, medication in enumerate([['On'], ['Off', 'Null']]):
+            df_ = df.query('upd23b_clinical_state_on_medication in @medication')
+            for j, part in enumerate(parts):
+                fig.add_trace(go.Box(x=df_['visit_month'], y=df_[f'updrs_{part}']), row=j+1, col=i+1)
+                fig.update_xaxes(title_text='Visit Month', row=j+1, col=i+1)
+                fig.update_yaxes(title_text=f'UPDRS Part {part}', row=j+1, col=1)
+        fig.update_layout(template='plotly_dark',
+                          width=850, height=2000,
+                          title_text='<b>UPDRS Score - Visit Month',
+                          showlegend=False)
+        fig.show()
 
 
 
+class DataPrep(object):
+    pass
 
 class CustomDataset(Dataset):
     def __init__(self, df):
